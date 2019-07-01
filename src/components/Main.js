66b99a1aa0ff241f9bcd50cb15ac48464b90f8ca
James Pagan-Lodge <stixx_60@hotmail.com>
2019-07-01 22:41:55 +0100

    Add grid component to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Grid from './Grid'
 
 class Main extends React.Component {
     constructor() {
@@ -12,7 +13,7 @@ class Main extends React.Component {
         return(
             <div>
                 <h1>The Game of Life</h1>
-                {/* <Grid /> */}
+                <Grid />
                 <h2>Generations: {this.state.generation}</h2>
             </div>
         )
