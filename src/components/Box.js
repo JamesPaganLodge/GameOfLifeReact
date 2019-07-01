408645cc5d643e509ad4322eff9f07441a8a820f
James Pagan-Lodge <stixx_60@hotmail.com>
2019-07-01 23:14:36 +0100

    Initial commit

---
+++ src/components/Box.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Box extends Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>
+        BOX
+      </div>
+    )
+  }
+}
+
+export default Box
