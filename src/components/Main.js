b010b031d51ccb8e65d5f87b18cb2b2ee57da09c
James Pagan-Lodge <stixx_60@hotmail.com>
2019-07-01 23:12:56 +0100

    Pass down properties to Grid component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,27 +2,31 @@ import React from 'react'
 import Grid from './Grid'
 
 class Main extends React.Component {
-    constructor() {
-        super()
-        this.speed = 100
-        this.rows = 30
-        this.cols = 50
+  constructor() {
+    super()
+    this.speed = 100
+    this.rows = 30
+    this.cols = 50
 
-        this.state = {
-            generation: 0,
-            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
-        }
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
+  }
 
-    render() {
-        return(
-            <div>
-                <h1>The Game of Life</h1>
-                <Grid />
-                <h2>Generations: {this.state.generation}</h2>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div>
+        <h1>The Game of Life</h1>
+        <Grid 
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    )
+  }
 }
 
 export default Main
